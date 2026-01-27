10f0ad982433d8537c1e080efe2fb679affee638
kasia.kordys <kasia.kordys@gmail.com>
2026-01-27 20:53:55 +0200

    diplay parking spots with status based on the user selection

--- parking-management/src/app/pages/dashboard/dashboard.component.ts
+++ parking-management/src/app/pages/dashboard/dashboard.component.ts
@@ -1,23 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IBuilding, IFloor, ISite } from '../../interfaces/master.interface';
 import { SitesService } from '../../services/sites.service';
 
+// parking-layout
+type SpotStatus = 'available' | 'occupied' | 'reserved' | 'maintenance';
+
+interface Spot {
+  id: string; // A1
+  status: SpotStatus;
+}
 @Component({
   selector: 'app-dashboard',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
 export class DashboardComponent implements OnInit {
   sitesService = inject(SitesService);
 
+  spots: Spot[] = [];
   siteData: ISite[] = [];
   buildingData: IBuilding[] = [];
   floorData: IFloor[] = [];
   selectedSiteId: number | null = null;
   selectedBuildingId: number | null = null;
   selectedFloorId: number | null = null;
+  totalParkingSpots: number = 0;
 
   ngOnInit(): void {
     this.sitesService.getSitesByClientId().subscribe({
@@ -31,13 +41,31 @@ export class DashboardComponent implements OnInit {
     });
   }
 
+  generateRandomSpots(total: number) {
+    const statuses: SpotStatus[] = [
+      'available',
+      'occupied',
+      'reserved',
+      'maintenance',
+    ];
+
+    this.spots = [];
+
+    for (let i = 1; i <= total; i++) {
+      this.spots.push({
+        id: `A${i}`,
+        status: statuses[Math.floor(Math.random() * statuses.length)],
+      });
+    }
+    console.log('Generated spots:', this.spots);
+  }
+
   onSiteChange(siteId: number | null): void {
     const selectedSite = this.siteData.find((site) => site.siteId === siteId);
     console.log('Selected site:', selectedSite);
 
     this.sitesService.GetBuildingBySiteId(siteId!).subscribe({
       next: (response) => {
-        debugger;
         this.buildingData = response.data;
         console.log('Buildings data for site ID', siteId, ':', response.data);
       },
@@ -59,7 +87,6 @@ export class DashboardComponent implements OnInit {
     console.log('Selected building:', selectedBuilding);
     this.sitesService.GetFloorsByBuildingId(buildingId!).subscribe({
       next: (response) => {
-        debugger;
         this.floorData = response.data;
         console.log(
           'Floors data for building ID',
@@ -77,5 +104,15 @@ export class DashboardComponent implements OnInit {
         );
       },
     });
+  }
+
+  onFloorChange(): void {
+    const floor = this.floorData.find(
+      (floor) => floor.floorId === this.selectedFloorId,
+    );
+
+    this.totalParkingSpots = floor?.totalParkingSpots ?? 0;
+
+    this.generateRandomSpots(this.totalParkingSpots);
   }
 }
